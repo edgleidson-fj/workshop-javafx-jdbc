003719edb99bccdea464a385b8b6c082da4cc310
Edgleidson <wake-up88@hotmail.com>
2020-07-07 18:51:18 -0300

    Design da Tela MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,9 @@
 package application;
-//Aula 267 - Tela MainView.	
+//Aula 268 -Design da Tela MainView.	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +11,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {														 //Diretório da View.		
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollPane = loader.load();
+			
+			//Código para deixar ScrollPane ajustado a janela.
+			scrollPane.setFitToHeight(true); //Altura.
+			scrollPane.setFitToWidth(true); //Largura.
+			
+			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Exemplo de Aplicação JavaFX");
 			primaryStage.show();
