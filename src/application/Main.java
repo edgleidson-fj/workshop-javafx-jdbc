08f57867e24d194a6fd3f20c626406a5d5937902
Edgleidson <wake-up88@hotmail.com>
2020-07-08 19:21:15 -0300

    MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-//Aula 268 -Design da Tela MainView.	
+//Aula 269 - MainViewController.	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
